5d2884f64c98c1865eced31dd4c9bea63362b174
kyung sejun <mlexp@naver.com>
2011-07-01 03:14:43 +0900

    kyung

---
+++ myCmdBase/cmd2.cpp
@@ -0,0 +1,15 @@
+#include "cmdTest.h"
+#include <conio.h>
+
+
+int cmd2(int argc, char (*argv)[ARG_SIZE]){
+	cout<<"Kyung se jun"<<endl;
+	cout<<"liquidus"<<endl;
+	cout<<"please any key..";
+	getch();
+	while(1){
+		cout<<".";
+	}
+		
+	return 0;
+}
